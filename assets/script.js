196e3f64b25ea3314fcbc6c41c47ae47c72c27d7
Zachary Dowell <zdowell97@gmail.com>
2022-02-15 20:24:37 -0500

    first commit

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,70 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Zachary Dowell" 
+let gb = 0    
+let cc = 0      
+let sugar = 0   
+document.getElementById('credit').textContent = `Created by ${yourName}`
 
-// We'll use these variables to track the counts of each cookie type
-let gb = 0 // Gingerbread
-let cc = 0 // Chocolate Chip
-let sugar = 0 // Sugar Sprinkle
 
-// selecting the element with an id of credit
-const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
 
-// Code to update name display
-credit.textContent = `Created by ${yourName}`
-
-// Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
-
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+document.getElementById('add-gb').addEventListener('click', function() {
+    let plusButton =document.querySelector('.add-gb');
+    
+        gb += 1;
+        document.getElementById('qty-gb').textContent = gb;
+        document.getElementById('qty-total').textContent= gb + cc+ sugar;
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+
+document.getElementById('minus-gb').addEventListener('click', function() {
+    if(gb > 0) {
+    gb -= 1;
+    }
+        document.getElementById('qty-gb').textContent = gb;
+        document.getElementById('qty-total').textContent= gb + cc+ sugar;
+       
+        
+})
+
+
+document.getElementById('add-cc').addEventListener('click', function(){
+    let plusButton =document.querySelector('.add-cc');
+    cc +=1;
+    document.getElementById('qty-cc').textContent = cc;
+    document.getElementById('qty-total').textContent= gb + cc+ sugar;
+    
+
+})
+
+
+document.getElementById('minus-cc').addEventListener('click', function() {
+    
+    if(cc > 0) {
+        cc -= 1;
+    }
+        document.getElementById('qty-cc').textContent = cc;
+        document.getElementById('qty-total').textContent= gb + cc+ sugar;
+       
+      
+})
+
+
+document.getElementById('add-sugar').addEventListener('click', function(){
+    let plusButton =document.querySelector('.add-sugar');
+    sugar +=1 ;
+    document.getElementById('qty-sugar').textContent = sugar;
+    document.getElementById('qty-total').textContent= gb + cc+ sugar;
+    
+   
+})
+
+
+document.getElementById('minus-sugar').addEventListener('click', function() {
+  
+    if(sugar > 0) {
+        sugar -= 1;
+    }
+        document.getElementById('qty-sugar').textContent = sugar;
+        document.getElementById('qty-total').textContent= gb + cc+ sugar;
+       
+    
+})
